1fab11f017ae11693d5197aad6ff0c1dc2b8acb9
ArathiRN <arathirn@gmail.com>
2024-07-12 20:04:59 -0700

    simple Spring Boot Hibernate APIs to manage a tutorial store application and store the details in an RDBMS using the Spring Data JPA library and SQL, for a tutorial application

---
+++ src/test/java/com/arathin/springmysql/SpringMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arathin.springmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
